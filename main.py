72f97427f286d0ae5c9c9a16b2c6e11842c03efb
HreshchyshynT <hreshchyshyn.t@gmail.com>
2025-05-26 15:43:51 +0300

    add sprite classe for circle and player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 import constants
+from player import Player
 
 
 def main():
@@ -12,13 +13,22 @@ def main():
     screen = pygame.display.set_mode(
         size=(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT),
     )
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(
+        constants.SCREEN_WIDTH / 2,
+        constants.SCREEN_HEIGHT / 2,
+    )
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color=pygame.Color(0, 0, 0))
+        screen.fill("black")
+
+        player.draw(screen)
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
